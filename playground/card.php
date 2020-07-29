a453a8591476a89ac742907fa4476fa8c668f254
Abraham Olaobaju <olaobajua@gmail.com>
2020-07-29 18:24:24 +0100

    card flow update

--- playground/card.php
+++ playground/card.php
@@ -6,58 +6,59 @@ include('partials/header.php');//this is just to load the bootstrap and css.
 DEFINE('DS', DIRECTORY_SEPARATOR);
 
 require("../library/CardPayment.php");
+require("testcards.php");
 use Flutterwave\Card;
 //The data variable holds the payload
-$data = array(
-    "card_number"=> "5531886652142950",
-            "cvv"=> "564",
-            "expiry_month"=> "09",
-            "expiry_year"=> "22",
-            "currency"=> "NGN",
-            "amount"=> "1000",
-            "fullname"=> "Ekene Eze",
-            "email"=> "ekene@flw.com",
-            "phone_number"=> "0902620185",
-            "fullname"=> "temi desola",
-            "tx_ref"=> "MC-3243e",// should be unique for every transaction
-            "redirect_url"=> "https://webhook.site/3ed41e38-2c79-4c79-b455-97398730866c",
-            "authorization"=> [
-                "mode"=> "pin",
-                "pin"=> "3310",
-                "country"=> "NG"
-            ]
-    );
+// $cards['card1'];
+// $cards['card2'];
 
 $payment = new Card();
 
-$result = $payment->cardCharge($data);
-$sera = serialize($payment);
+if (isset($_POST['charge'])){
 
-$filePath = getcwd().DS."payment.txt";
-if (is_writable($filePath)) {
-    $fp = fopen($filePath, "w"); 
-    fwrite($fp, $sera); 
-    fclose($fp);
+
+    $card_option = $_POST['token'];
+
+    $result = $payment->cardCharge($cards[$card_option]);
+    $sera = serialize($payment);
+
+    $filePath = getcwd().DS."payment.txt";
+    if (is_writable($filePath)) {
+        $fp = fopen($filePath, "w"); 
+        fwrite($fp, $sera); 
+        fclose($fp);
+        
+    }
+    $url = 'otp2.php?ref='.$result['data']['flw_ref'];
+        header( 'Location:'.$url);
+
+
 }
-
-echo '<div class="alert alert-success role="alert">
-        <h1>Charge Result: </h1>
-        <p><b> '.print_r($result, true).'</b></p>
-      </div>';
-
-
-
-//validating the charge by Entering otp.....
-echo '<iframe src = "otp2.php?ref='.$result['data']['flw_ref'].'" width = "100%" height = "400" frameBorder="0">
-Sorry your browser does not support inline frames.
-</iframe>';
-
-
-
-
 
 include('partials/footer.php');//this is just to load the jquery and js scripts. 
 
 ?>
+
+<div class="container" style="text-align:center">
+<h1>Card payment Implementation </h1>
+<ul class="list-group col-md-6"><li class="list-group-item">
+                                <form method="POST" action="">
+                                    MasterCard/Verve - Pin
+                                    <input type="hidden" name="token" value="card1">
+                                    <input type="submit" value="Charge" class="btn btn-sm btn-success float-right" name="charge">
+                                </form>
+                            </li><li class="list-group-item">
+                                <form method="POST" action="">
+                                    NO_auth International Card - No_auth
+                                    <input type="hidden" name="token" value="card2">
+                                    <input type="submit" value="Charge" class="btn btn-sm btn-success float-right" name="charge">
+                                </form>
+                            </li></ul> 
+
+    </div>
+
+
+
+
 
 
