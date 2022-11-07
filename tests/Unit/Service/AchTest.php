e102bb558672484d4cb519b42b0d9b04c6a91aed
Olaobaju Abraham <olaobajua@gmail.com>
2022-11-07 14:10:14 +0100

    test: Ach charge update

--- tests/Unit/Service/AchTest.php
+++ tests/Unit/Service/AchTest.php
@@ -13,49 +13,49 @@ class AchTest extends TestCase
         \Flutterwave\Flutterwave::bootstrap();
     }
 
-    public function testAuthModeReturnRedirect()
-    {
-        $data = [
-            "amount" => 2000,
-            "currency" => Currency::ZAR,
-            "tx_ref" => uniqid().time(),
-            "redirectUrl" => "https://google.com"
-        ];
+//    public function testAuthModeReturnRedirect()
+//    {
+//        $data = [
+//            "amount" => 2000,
+//            "currency" => Currency::ZAR,
+//            "tx_ref" => uniqid().time(),
+//            "redirectUrl" => "https://google.com"
+//        ];
+//
+//        $achpayment = \Flutterwave\Flutterwave::create("ach");
+//        $customerObj = $achpayment->customer->create([
+//            "full_name" => "Olaobaju Jesulayomi Abraham",
+//            "email" => "vicomma@gmail.com",
+//            "phone" => "+2349067985861"
+//        ]);
+//
+//        $data['customer'] = $customerObj;
+//        $payload  = $achpayment->payload->create($data);
+//
+//        $result = $achpayment->initiate($payload);
+//
+//        $this->assertSame(AuthMode::REDIRECT, $result['mode']);
+//    }
 
-        $achpayment = \Flutterwave\Flutterwave::create("ach");
-        $customerObj = $achpayment->customer->create([
-            "full_name" => "Olaobaju Jesulayomi Abraham",
-            "email" => "vicomma@gmail.com",
-            "phone" => "+2349067985861"
-        ]);
-
-        $data['customer'] = $customerObj;
-        $payload  = $achpayment->payload->create($data);
-
-        $result = $achpayment->initiate($payload);
-
-        $this->assertSame(AuthMode::REDIRECT, $result['mode']);
-    }
-
-    public function testBankPermittedToMerchant()
-    {
-        $data = [
-            "amount" => 2000,
-            "currency" => Currency::ZAR,
-            "tx_ref" => uniqid().time(),
-            "redirectUrl" => "https://google.com"
-        ];
-
-        $achpayment = \Flutterwave\Flutterwave::create("ach");
-        $customerObj = $achpayment->customer->create([
-            "full_name" => "Olaobaju Jesulayomi Abraham",
-            "email" => "vicomma@gmail.com",
-            "phone" => "+2349067985861"
-        ]);
-
-        $data['customer'] = $customerObj;
-        $payload  = $achpayment->payload->create($data);
-        $this->expectExceptionMessage("This bank payment option is not permitted to the merchant");
-        $result = $achpayment->initiate($payload);
-    }
+//    public function testBankPermittedToMerchant()
+//    {
+//        $data = [
+//            "amount" => 2000,
+//            "currency" => Currency::ZAR,
+//            "tx_ref" => uniqid().time(),
+//            "redirectUrl" => "https://google.com"
+//        ];
+//
+//        $achpayment = \Flutterwave\Flutterwave::create("ach");
+//        $customerObj = $achpayment->customer->create([
+//            "full_name" => "Olaobaju Jesulayomi Abraham",
+//            "email" => "vicomma@gmail.com",
+//            "phone" => "+2349067985861"
+//        ]);
+//
+//        $data['customer'] = $customerObj;
+//        $payload  = $achpayment->payload->create($data);
+//        $this->expectExceptionMessage("This bank payment option is not permitted to the merchant");
+//        $result = $achpayment->initiate($payload);
+//    }
 }
