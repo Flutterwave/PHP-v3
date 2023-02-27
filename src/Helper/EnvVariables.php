32cbd7a0e9f6c08d99f6c1f20d3a97a26ec0823c
Olaobaju Abraham <39011309+bajoski34@users.noreply.github.com>
2023-02-27 18:08:59 +0000

    remove BASE_URL variable from config implementation

---
+++ src/Helper/EnvVariables.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Flutterwave\Helper;
+
+class EnvVariables
+{
+    public const VERSION = 'v3';
+    public const BASE_URL = 'https://api.flutterwave.com/'.self::VERSION;
+    public const TIME_OUT = 30;
+}
