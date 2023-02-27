04bd561b7825c526e400f202a545d0cc60e23129
Olaobaju Abraham <39011309+bajoski34@users.noreply.github.com>
2023-02-27 18:03:10 +0000

    handle Network, Client and Request exceptions

--- src/Exception/AuthenticationException.php
+++ src/Exception/AuthenticationException.php
@@ -6,4 +6,11 @@ namespace Flutterwave\Exception;
 
 class AuthenticationException extends \Exception
 {
+    public function InvalidBearerToken() {
+        $this->message = "Invalid Secret Key passed.";
+    }
+
+    public function UnauthorizedAccess() {
+        $this->message = "You currently do not have permission to access this feature. kindly reachout to the Account owner.";
+    }
 }
---
+++ src/Exception/ClientException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Flutterwave\Exception;
+
+use Psr\Http\Client\ClientExceptionInterface;
+
+/**
+ * ClientException
+ */
+class ClientException extends \RuntimeException implements ClientExceptionInterface
+{
+
+}
