4df16455be48c454d08218747872812bbcf081db
Olaobaju Abraham <olaobajua@gmail.com>
2023-03-02 22:18:42 +0100

    update: ApiOperation get trait

--- src/Traits/ApiOperations/Get.php
+++ src/Traits/ApiOperations/Get.php
@@ -5,20 +5,26 @@ declare(strict_types=1);
 namespace Flutterwave\Traits\ApiOperations;
 
 use Flutterwave\Contract\ConfigInterface;
+use Flutterwave\Exception\ApiException;
 use Flutterwave\Service\Service as Http;
 use Psr\Http\Client\ClientExceptionInterface;
+use stdClass;
 
 trait Get
 {
     /**
+     * @param ConfigInterface $config
      * @param string $url
-     * @return string
+     * @return stdClass
      * @throws ClientExceptionInterface
+     * @throws ApiException
      */
-    public function getURL(ConfigInterface $config, string $url): string
+    public function getURL(ConfigInterface $config, string $url): stdClass
     {
         $response = (new Http($config))->request(null, 'GET', $url);
-
-        return '';
+        if ($response->status === 'success') {
+            return $response;
+        }
+        throw new ApiException($response->message);
     }
 }
