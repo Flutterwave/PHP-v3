efc50ae1f657493a980ac3638a303beef58f63e4
Abraham Olaobaju <abraham@flutterwave.com>
2023-10-15 11:37:18 +0000

    add SDK_VERSION to environment variables

--- src/Helper/EnvVariables.php
+++ src/Helper/EnvVariables.php
@@ -7,6 +7,7 @@ namespace Flutterwave\Helper;
 class EnvVariables
 {
     public const VERSION = 'v3';
+    public const SDK_VERSION = '1.0.7';
     public const BASE_URL = 'https://api.flutterwave.com/' . self::VERSION;
     public const TIME_OUT = 30;
 }
